d28e36bbef72ae1400ee75d2c8a7b5de7e798c14
narmathaa07 <24004481@siswa.um.edu.my>
2026-05-02 03:33:40 +0800

    Add device power ratings and usage hours data

---
+++ data.py
@@ -0,0 +1,15 @@
+devices = {
+    "Air Conditioner": 1200,
+    "Fan": 60,
+    "TV": 100,
+    "Fridge": 200,
+    "Light": 40
+}
+
+usage_hours = {
+    "Air Conditioner": 5,
+    "Fan": 8,
+    "TV": 3,
+    "Fridge": 24,
+    "Light": 6
+}
